e717bac420327eb2aeab84db4eb9eccfa43c5815
pedritto19 <pedrosusuke@gmail.com>
2024-03-20 10:49:56 -0300

    test: espaco

--- my-app/src/backend/server.ts
+++ my-app/src/backend/server.ts
@@ -13,7 +13,7 @@ const app= Fastify({ logger: true})
 const start= async() =>{
     
     
-    
+
 
     await app.register(cors);
     await app.register(routes);
@@ -21,7 +21,7 @@ const start= async() =>{
     
 
     try {
-        await app.listen({ port: 3333, host: '0.0.0.0' });
+        await app.listen({ port: 3333, host: '0.0.0.0' });    
         // Adicionando host: '0.0.0.0' faz com que o servidor aceite conexões em todos os endereços IPv4.
     } catch (err) {
         app.log.error(err);
