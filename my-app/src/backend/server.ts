5aa8754bfa23cd7473cb22146ec3ebb78b9038c7
pedritto19 <pedrosusuke@gmail.com>
2024-03-20 10:26:58 -0300

    test: vercel

--- my-app/src/backend/server.ts
+++ my-app/src/backend/server.ts
@@ -9,18 +9,23 @@ import App from '../App';
 
 const app= Fastify({ logger: true})
 
+
 const start= async() =>{
+    
+    
     
 
     await app.register(cors);
     await app.register(routes);
     
+    
 
-    try{
-        await app.listen({port: 3333})
-    }catch(err){
-        process.exit(1)
-
+    try {
+        await app.listen({ port: 3333, host: '0.0.0.0' });
+        // Adicionando host: '0.0.0.0' faz com que o servidor aceite conexões em todos os endereços IPv4.
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
     }
 }
 
