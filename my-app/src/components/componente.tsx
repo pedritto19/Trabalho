b983d0af96f93ca5cff9f01b420766a83d24e9c8
pedritto19 <pedrosusuke@gmail.com>
2024-04-04 12:18:40 -0300

    test: banner

--- my-app/src/components/componente.tsx
+++ my-app/src/components/componente.tsx
@@ -3,8 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import edicao from './images/edicao.png'
 import cadastro from './images/cadastro.png'
-import React, { useState, useEffect } from 'react';
-import topo from './images/topo.png'
+
 
 const TextoElegante = styled.p`
   font-family: 'Roboto', sans-serif;
@@ -21,25 +20,6 @@ const TextoElegante = styled.p`
 
     function Mensagem() {
 
-      const [showTopBtn, setShowTopBtn] = useState(false);
-
-      useEffect(() => {
-        window.addEventListener("scroll", () => {
-          if (window.scrollY > 0) {
-            setShowTopBtn(true); // Mostrar botão se a rolagem for maior que 300px
-          } else {
-            setShowTopBtn(false); // Esconder botão
-          }
-        });
-      }, []);
-    
-      // Função para voltar ao topo
-      const goToTop = () => {
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth' // Rolagem suave
-        });
-      };
 
     
     
@@ -64,11 +44,11 @@ const TextoElegante = styled.p`
 
     return (
         <div style={{ textAlign: 'center' }} className="container1" >
-          <div>
+        
           
           <h3><TextoElegante>Bem Vindo</TextoElegante></h3>
         <p><TextoElegante>Página inicial</TextoElegante></p>
-        <div className="menu-opcoes" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        
             <button  id="meuBotaoM" onClick={handleClick2}>
               <img src={cadastro} alt="Cadastrar" style={{ width: 'auto', height: '40px', transform: 'scaleX(-1)' }} />
               <span className="tooltip">Cadastrar Pokémon</span>
@@ -79,17 +59,13 @@ const TextoElegante = styled.p`
               <span className="tooltip">Editar Pokémon</span>
               
             </button>
-          </div>
+       
         
 
 
    
-      </div>
-      {showTopBtn && (
-        <button id='meuBotao2' onClick={goToTop} className="meuBotaoAnimado"  style={{ position: 'fixed', top: '20px', left: '48.2%'}}>
-          <img src={topo} alt="Voltar ao topo" style={{ width: '30px', height: 'auto' }} />
-        </button>
-      )}
+      
+
       </div>
     );
   }
