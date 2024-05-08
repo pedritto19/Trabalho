ce159b1462af989c3faa3a02ce2a6c49e8fe0549
pedritto19 <pedrosusuke@gmail.com>
2024-05-08 09:05:10 -0300

    test: ultimas

--- my-app/src/services/showTypeService.ts
+++ my-app/src/services/showTypeService.ts
@@ -3,7 +3,7 @@ import prismaClient from "../prisma";
 
 export class showTypeService {
     async listAlltype() {
-        const showtype = await prismaClient.type.findMany({
+        const showtype = await prismaClient.types.findMany({
 
         })
         return showtype;
