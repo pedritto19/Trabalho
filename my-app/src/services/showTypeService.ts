cdbacd5944699340fe72453799c6c424babae30b
pedritto19 <pedrosusuke@gmail.com>
2024-05-08 08:54:54 -0300

    test: service e schema

--- my-app/src/services/showTypeService.ts
+++ my-app/src/services/showTypeService.ts
@@ -2,15 +2,10 @@
 import prismaClient from "../prisma";
 
 export class showTypeService {
-  async listAlltype() {
-    try {
-      console.log(prismaClient); // Verifica se o PrismaClient está definido
-      console.log(prismaClient.pokemon); // Verifica se o modelo 'pokemon' é reconhecido
-      const types = await prismaClient.type.findMany();
-      return types;
-    } catch (error) {
-      console.error("Error listing types:", error);
-      throw error;
+    async listAlltype() {
+        const showtype = await prismaClient.type.findMany({
+
+        })
+        return showtype;
     }
-  }
 }
