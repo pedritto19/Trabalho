266ecd5fb8c7c1f0d34a686d524ae2ad83aa8674
pedritto19 <pedrosusuke@gmail.com>
2024-05-08 09:51:20 -0300

    test: atualizado cache

--- my-app/src/services/showTypeService.ts
+++ my-app/src/services/showTypeService.ts
@@ -3,10 +3,8 @@ import prismaClient from "../prisma";
 export class showTypeService {
     async listAlltype() {
         const showtype = await prismaClient.types.findMany({
-            select: {
-                name: true // Isso indica ao Prisma que apenas o campo 'name' deve ser retornado
-            }
-        });
+
+        })
         return showtype;
     }
 }
