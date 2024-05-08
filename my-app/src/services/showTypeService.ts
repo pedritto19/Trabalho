cfee95fcf9f0499391f2f481b5bba4362047064a
pedritto19 <pedrosusuke@gmail.com>
2024-05-08 09:22:03 -0300

    fix: find first

--- my-app/src/services/showTypeService.ts
+++ my-app/src/services/showTypeService.ts
@@ -3,7 +3,7 @@ import prismaClient from "../prisma";
 
 export class showTypeService {
     async listAlltype() {
-        const showtype = await prismaClient.types.findFirst({
+        const showtype = await prismaClient.types.findMany({
 
         })
         return showtype;
