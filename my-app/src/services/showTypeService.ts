79b3664c3225526c00b9824b01df1c1e9c78d136
pedritto19 <pedrosusuke@gmail.com>
2024-05-08 08:38:48 -0300

    test: service

--- my-app/src/services/showTypeService.ts
+++ my-app/src/services/showTypeService.ts
@@ -2,10 +2,15 @@
 import prismaClient from "../prisma";
 
 export class showTypeService {
-    async listAlltype() {
-        const showtype = await prismaClient.type.findMany({
-
-        })
-        return showtype;
+  async listAlltype() {
+    try {
+      console.log(prismaClient); // Deve mostrar o objeto PrismaClient
+      console.log(prismaClient.type); // Deve mostrar o modelo Type, não deve ser undefined
+      const types = await prismaClient.type.findMany();
+      return types;
+    } catch (error) {
+      console.error("Error listing types:", error);
+      throw error;
     }
+  }
 }
